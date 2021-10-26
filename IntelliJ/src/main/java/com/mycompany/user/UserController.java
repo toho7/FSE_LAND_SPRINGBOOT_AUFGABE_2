b91eb8636acba1202c04f1ac500a1c5a48ff1631
Thomas Hofmann <thofmann2104@gmail.com>
2021-10-26 23:25:18 +0200

    Code Form Validate

--- IntelliJ/src/main/java/com/mycompany/user/UserController.java
+++ IntelliJ/src/main/java/com/mycompany/user/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -23,5 +25,12 @@ public class UserController {
     public String showNewForm(Model model){
         model.addAttribute("user", new User());
         return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes ra){
+        service.save(user);
+        ra.addFlashAttribute("message", "The user has been saved successfully.");
+        return "redirect:/users";   //Weiterleitung auf Benutzer-Übersichtsseite
     }
 }
