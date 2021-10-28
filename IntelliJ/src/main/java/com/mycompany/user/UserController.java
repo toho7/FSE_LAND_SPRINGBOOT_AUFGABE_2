e91bf89c10802d8b6d1fa74b8d6ab28b57985206
Thomas Hofmann <thofmann2104@gmail.com>
2021-10-28 11:38:56 +0200

    Code Edit/Update User Function

--- IntelliJ/src/main/java/com/mycompany/user/UserController.java
+++ IntelliJ/src/main/java/com/mycompany/user/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,6 +25,7 @@ public class UserController {
     @GetMapping("/users/new")
     public String showNewForm(Model model){
         model.addAttribute("user", new User());
+        model.addAttribute("pageTitle", "Add New User");
         return "user_form";
     }
 
@@ -32,5 +34,18 @@ public class UserController {
         service.save(user);
         ra.addFlashAttribute("message", "The user has been saved successfully.");
         return "redirect:/users";   //Weiterleitung auf Benutzer-Übersichtsseite
+    }
+
+    @GetMapping("/users/edit/{id}")
+    public String showEditForm(@PathVariable("id") Integer id, Model model, RedirectAttributes ra){
+        try {
+            User user = service.get(id);
+            model.addAttribute("user", user);
+            model.addAttribute("pageTitle", "Edit User (ID: "+id+" )");
+            return "user_form";
+        } catch (UserNotFoundException e) {
+            ra.addFlashAttribute("message", e.getMessage());
+            return "redirect:/users";
+        }
     }
 }
